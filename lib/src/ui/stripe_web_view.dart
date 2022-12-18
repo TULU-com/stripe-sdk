919a8dc2f52f88d1c1fa604b5abc85fdbbbcbfcc
tamirrab <tamir.rabia@gmail.com>
2022-12-18 13:18:36 +0200

    Update stripe_web_view.dart
    Upgrade WebViewWidget to version 3.0

--- lib/src/ui/stripe_web_view.dart
+++ lib/src/ui/stripe_web_view.dart
@@ -1,26 +1,45 @@
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class StripeWebView extends StatelessWidget {
-  const StripeWebView({Key? key, required this.uri, required this.returnUri}) : super(key: key);
+class StripeWebView extends StatefulWidget {
+  const StripeWebView({Key? key, required this.uri, required this.returnUri})
+      : super(key: key);
 
   final String uri;
   final Uri returnUri;
 
   @override
+  State<StripeWebView> createState() => _StripeWebViewState();
+}
+
+class _StripeWebViewState extends State<StripeWebView> {
+  WebViewController _webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    _webViewController = WebViewController()
+      ..loadRequest(Uri.parse(widget.uri))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(NavigationDelegate(
+          onNavigationRequest: (NavigationRequest navigation) {
+        final uri = Uri.parse(navigation.url);
+        if (uri.scheme == widget.returnUri.scheme &&
+            uri.host == widget.returnUri.host &&
+            uri.queryParameters['requestId'] ==
+                widget.returnUri.queryParameters['requestId']) {
+          Navigator.pop(context, true);
+          return NavigationDecision.prevent;
+        }
+        return NavigationDecision.navigate;
+      }));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return WebView(
-        initialUrl: uri,
-        javascriptMode: JavascriptMode.unrestricted,
-        navigationDelegate: (navigation) {
-          final uri = Uri.parse(navigation.url);
-          if (uri.scheme == returnUri.scheme &&
-              uri.host == returnUri.host &&
-              uri.queryParameters['requestId'] == returnUri.queryParameters['requestId']) {
-            Navigator.pop(context, true);
-            return NavigationDecision.prevent;
-          }
-          return NavigationDecision.navigate;
-        });
+    return WebViewWidget(
+      controller: _webViewController,
+    );
   }
 }
